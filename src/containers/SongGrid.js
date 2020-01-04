d05ab3aeb3914bd928326fa22bff793b8feaf7b8
Anish Shah <anish.shah01@sap.com>
2020-01-04 03:59:36 -0800

    Update SongGrid.js
    Changed orientation of song images

--- src/containers/SongGrid.js
+++ src/containers/SongGrid.js
@@ -12,15 +12,16 @@ import _ from 'lodash';
 
 const styles = StyleSheet.create({
     root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden'
-    },
-    gridList: {
-      width: "100%",
-      height: "100%",
-    }
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+      },
+      gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+      }
   })
 
 //Send request for track details here? or in main container. probs main container for now lmao
@@ -67,9 +68,9 @@ export class SongGrid extends Component {
                 {/* Display tracks in cards */}
                 <div className ={css(styles.root)}>
                     <GridList cellHeight={180} cols={6} className={css(styles.gridList)}>
-                        <GridListTile key="Subheader" cols={6} style={{ height: 'auto' }}>
+                        {/* <GridListTile key="Subheader" cols={6} style={{ height: 'auto' }}>
                             <ListSubheader component="div">{this.props.playlist.name}</ListSubheader>
-                        </GridListTile>
+                        </GridListTile> */}
                         {tracks.map(track => 
                             <GridListTile key={track.track.id} cols={1}>
                                 <img src={track.track.album.images[0] ? track.track.album.images[0].url : ""} alt={track.track.name} />
